4f58054ca4f3172fa785850cc5e32967dc5267c9
N.Mathyukan <122957559+Mathyukan98@users.noreply.github.com>
2023-02-04 20:01:46 +0530

    Add files via upload

---
+++ whiteboard-master/public/apidoc/api_project.js
@@ -0,0 +1,14 @@
+define({
+  "name": "whiteboard",
+  "version": "1.0.0",
+  "description": "Collaborative Whiteboard / Sketchboard",
+  "sampleUrl": false,
+  "defaultVersion": "0.0.0",
+  "apidoc": "0.3.0",
+  "generator": {
+    "name": "apidoc",
+    "time": "2021-11-25T07:22:23.446Z",
+    "url": "https://apidocjs.com",
+    "version": "0.26.0"
+  }
+});
